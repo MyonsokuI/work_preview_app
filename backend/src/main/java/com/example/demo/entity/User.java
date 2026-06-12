b02de2244146240847662d055a2df6b2355a0ade
WIN-FJOK69KA9AP\traininguser <rintaro.yasutake@skywill.jp>
2026-06-12 17:24:51 +0900

    refactor: Add CreatedDate and LastModifiedDate annotations to User entity

--- backend/src/main/java/com/example/demo/entity/User.java
+++ backend/src/main/java/com/example/demo/entity/User.java
@@ -2,6 +2,8 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import com.example.demo.entity.enums.Role;
 import com.example.demo.entity.enums.UserStatus;
